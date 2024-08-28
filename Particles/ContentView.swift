7066cf7d27ff2759d41286a65d814d0c077d733b
Florian Kugler <mail@floriankugler.com>
2024-08-28 17:40:54 +0200

    Initial commit

---
+++ Particles/ContentView.swift
@@ -0,0 +1,72 @@
+//
+
+import SwiftUI
+
+struct ParticleModifier<T: Hashable>: ViewModifier {
+    var trigger: T
+
+    @State var angle = Angle.degrees(.random(in: 0...360))
+
+    var distance: Double = 40
+    var offset: CGSize {
+        .init(width: cos(angle.radians) * distance, height: sin(angle.radians) * distance)
+    }
+
+    var t: AnyTransition {
+        .asymmetric(insertion: .identity, removal: .offset(offset)
+            .combined(with: .opacity))
+    }
+
+    func body(content: Content) -> some View {
+        ZStack {
+            content
+                .transition(t)
+                .id(trigger)
+        }
+        .animation(.default.speed(0.2), value: trigger)
+        .onChange(of: trigger) {
+            angle = Angle.degrees(.random(in: 0...360))
+        }
+    }
+}
+
+extension View {
+    func sprayEffect<Trigger: Hashable>(trigger: Trigger) -> some View {
+        self.background {
+            ZStack {
+                ForEach(0..<30) { _ in
+                    self
+                        .modifier(ParticleModifier(trigger: trigger))
+                }
+            }
+        }
+    }
+}
+
+struct ContentView: View {
+    @ScaledMetric var dividerHeight = 18
+    @State private var trigger = 0
+    var body: some View {
+        VStack {
+            Button(action: {
+                trigger += 1
+            }, label: {
+                HStack {
+                    Image(systemName: "star.fill")
+                        .sprayEffect(trigger: trigger)
+                    Divider()
+                        .frame(height: dividerHeight)
+                    Text("Favorite")
+                }
+                .contentShape(.rect)
+            })
+        }
+        .buttonStyle(.link)
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+        .padding(50)
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# Swift Talk
+## Particle Effecs (Part 1)
+
+This is the code that accompanies Swift Talk Episode 418: [Particle Effecs (Part 1)](https://talk.objc.io/episodes/S01E418-particle-effects-part-1)
